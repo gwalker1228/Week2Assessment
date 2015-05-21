aea06de01d2ffbe22d7a22a5df03db51aab9a425
Gretchen Walker <gwalker1228@gmail.com>
2015-05-21 14:23:31 -0700

    Initial Commit

---
+++ Week2Assessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week2Assessment
+//
+//  Created by Gretchen Walker on 5/21/15.
+//  Copyright (c) 2015 Gretchen Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Week2Assessment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Week2Assessment
+//
+//  Created by Gretchen Walker on 5/21/15.
+//  Copyright (c) 2015 Gretchen Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
